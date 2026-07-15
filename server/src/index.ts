0c6bd3eeca8b081caef377178dcd7b5979590488
FranciscoAguero247 <blast255@live.com>
2026-07-14 18:01:56 -0600

    fix frontend's Render domain

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173', 
-    'https://culinary-api-backend-zgwt.onrender.com'
+    'https://culinary-api-frontend.onrender.com'
   ]
 }));
 
